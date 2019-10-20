1bde6880127e2a1105e34e165becd7fef46eff5b
Eduardo Urbaez <eurbaezjr@gmail.com>
2019-10-19 23:29:39 -0700

    HW revised

--- code.js
+++ code.js
@@ -5,6 +5,22 @@ var date = moment().format("dddd, MMMM Do");
 var nowHour = moment().format("HH");
 $("#currentDay").text(date);
 
+if (lastSaved === undefined || lastSaved  == null || lastSaved.length <= 0) {
+var user = {};}
+else {
+    var user = {
+        input9: lastSaved.input9,
+        input10: lastSaved.input10,
+        input11: lastSaved.input11,
+        input12: lastSaved.input12,
+        input13: lastSaved.input13,
+        input14: lastSaved.input14,
+        input15: lastSaved.input15,
+        input16: lastSaved.input16,
+        input17: lastSaved.input17,
+        };
+}
+
 // =================================================
 // Dynamically change color of boxes depending on time
 
@@ -26,55 +42,36 @@ else if (parseInt($(this).attr("id")) == parseInt(nowHour))
 $("button").on("click", function(){
 var savedHour = $(this).attr('value');
 
-var user = {
-input9: lastSaved.input9,
-input10: lastSaved.input10,
-input11: lastSaved.input11,
-input12: lastSaved.input12,
-input13: lastSaved.input13,
-input14: lastSaved.input14,
-input15: lastSaved.input15,
-input16: lastSaved.input16,
-input17: lastSaved.input17,
-};
-
 // Link the button clicked to save to local storage
 if (savedHour == 9) { 
 user.input9 = $("#9").val();
-localStorage.setItem("user",JSON.stringify(user));
 }
 else if (savedHour == 10) { 
 user.input10 = $("#10").val();
-localStorage.setItem("user",JSON.stringify(user));
 }
 else if (savedHour == 11) { 
 user.input11 = $("#11").val();
-localStorage.setItem("user",JSON.stringify(user));
 }
 else if (savedHour == 12) { 
 user.input12 = $("#12").val();
-localStorage.setItem("user",JSON.stringify(user));
 }
 else if (savedHour == 13) { 
 user.input13 = $("#13").val();
-localStorage.setItem("user",JSON.stringify(user));
 }
 else if (savedHour == 14) { 
 user.input14 = $("#14").val();
-localStorage.setItem("user",JSON.stringify(user));
 }
 else if (savedHour == 15) { 
 user.input15 = $("#15").val();
-localStorage.setItem("user",JSON.stringify(user));
 }
 else if (savedHour == 16) { 
 user.input16 = $("#16").val();
-localStorage.setItem("user",JSON.stringify(user));
 }
 else if (savedHour == 17) { 
 user.input17 = $("#17").val();
-localStorage.setItem("user",JSON.stringify(user));
 }
+
+localStorage.setItem("user",JSON.stringify(user));
 
 });
 // Return saved items from local storage
